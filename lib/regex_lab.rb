9faec2e8c9751f8c88e9f67df6fc74e23c251e7f
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-27 15:02:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,7 @@
 def starts_with_a_vowel?(word)
-  if (word.match(/^[aieou]/)
-    return true
-  else
-    return false
-  end
-end
+return (word.scan(/^[aieou]/)
+
+
 
 def words_starting_with_un_and_ending_with_ing(text)
 
