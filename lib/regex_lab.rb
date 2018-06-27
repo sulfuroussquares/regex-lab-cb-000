fca3cb425d4401a5bbdf3a0d327cc266e4ddbfa8
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-27 17:03:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,5 +20,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  return phone.scan(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/).empty? ? false : true
 end
