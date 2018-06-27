8f3093b6d4d3d73b0de9feb3abbff0f34a82f8ca
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-27 17:02:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ return splitArray.grep(/\b\w{5}\b/).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  splutArray = text.split(" ")
+  splitArray = text.split(" ")
   return splitArray.grep(/\A[A-Z].+[?.,!]$/).empty? ? false : true
 end
 
